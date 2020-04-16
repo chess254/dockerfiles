253a0939befcca932ddc31ff208f85b0423bd1c0
RubenAshughyan <ruben.ashughyan@gmail.com>
2020-04-16 16:11:14 +0400

    use custom shell script file

--- base/docker_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "${0%/*}"
-
-tag="latest"
-
-docker build -t codesignal/ubuntu-base:dev .
-#docker build --no-cache -t baseimage:${tag} .
---
+++ build_updated_dockerfiles_dev.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo Starting
+
+declare -A REPOMAP
+REPOMAP[r]='r-env'
+REPOMAP[d]='d-env'
+REPOMAP[base]='ubuntu-base'
+
+echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+
+echo ---DOCKERHUB_PASSWORD $DOCKERHUB_PASSWORD
+echo ---DOCKERHUB_USERNAME $DOCKERHUB_USERNAME
+echo --PWD
+pwd
+ls
+echo ------Logged Into docker
+
+echo ------List of updated files
+FILES=$(git diff-tree --name-only -r origin/master HEAD)
+for F in $FILES; do echo $F; done
+
+exit 0
+
+REGEX="(.*)\/Dockerfile"
+for F in $FILES
+do
+  if [[ $F =~ $REGEX ]]
+  then
+    IMAGE=${BASH_REMATCH[1]}
+
+    if [ ${REPOMAP[${IMAGE}]+_} ]
+    then
+      REPO=${REPOMAP[${IMAGE}]}
+    else
+      REPO=$IMAGE
+    fi
+
+    echo dockerfile: $IMAGE '->' repo: $REPO
+    docker build -t codesignal/$REPO:dev $IMAGE
+    #docker push codesignal/$REPO:dev
+  fi
+  echo --------------------------------------------------------------------
+done
