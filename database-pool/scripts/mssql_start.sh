8c42915455ff36ef214e3e8a3daa55f124569851
Michał Powaga <michalpowaga13@gmail.com>
2021-05-20 15:30:02 +0100

    Create test user when starting mssql server

--- database-pool/scripts/mssql_start.sh
+++ database-pool/scripts/mssql_start.sh
@@ -8,10 +8,30 @@ mssql_ready() {
   /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$MSSQL_SA_PASSWORD" -l1 -Q "SELECT @@VERSION" > /dev/null 2>&1
 }
 
-ACCEPT_EULA=Y MSSQL_PID="$MSSQL_PID" MSSQL_SA_PASSWORD="$MSSQL_SA_PASSWORD" nohup /opt/mssql/bin/sqlservr > /dev/null 2>&1 &
+SQLSERVR_STARTED=0
+if !(mssql_ready); then
+  ACCEPT_EULA=Y MSSQL_PID="$MSSQL_PID" MSSQL_SA_PASSWORD="$MSSQL_SA_PASSWORD" nohup /opt/mssql/bin/sqlservr > /dev/null 2>&1 &
+  SQLSERVR_STARTED=1
+fi
 
 while !(mssql_ready)
 do
   sleep .5
   echo "Waiting for sqlservr..."
 done
+
+if [ $SQLSERVR_STARTED -eq 1 ]; then
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$MSSQL_SA_PASSWORD" <<END >/dev/null
+CREATE DATABASE test;
+GO
+USE test;
+
+CREATE LOGIN test WITH PASSWORD = 'test', CHECK_POLICY = OFF;
+CREATE USER test FOR LOGIN test;
+GRANT CONTROL ON DATABASE::test TO test;
+-- sysadmin role is required for raiserror with log command
+-- which we use in the template to terminate the execution
+exec sp_addsrvrolemember 'test', 'sysadmin';
+GO
+END
+fi
