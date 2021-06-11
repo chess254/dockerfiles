6457fcdf47e2f0dd749a3489a0c50231223dac96
Michał Powaga <michalpowaga13@gmail.com>
2021-06-11 11:20:28 +0100

    Update postgresql config to allow local connections

--- database-pool/scripts/postgresql_start.sh
+++ database-pool/scripts/postgresql_start.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env sh
 
-# Allow remote connections
-LINE='host  all  all 0.0.0.0/0 trust'
-FILE='/etc/postgresql/13/main/pg_hba.conf'
-grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
-echo "listen_addresses = '*'" > /etc/postgresql/13/main/conf.d/my.conf
-
 service postgresql start > /dev/null 2>&1
 
 su - postgres << EOF
